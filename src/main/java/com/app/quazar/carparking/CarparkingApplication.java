25a21367acd652799a6b451a405357cee230675b
Nickolas Alexandre <nickolaz.0603@gmail.com>
2022-01-14 21:46:31 -0300

    start project

---
+++ src/main/java/com/app/quazar/carparking/CarparkingApplication.java
@@ -0,0 +1,13 @@
+package com.app.quazar.carparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/quazar/carparking/CarparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quazar.carparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
